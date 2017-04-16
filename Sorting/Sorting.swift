076034582bae42f1deef5d3c5d314d2d83efd0dd
ayman003 <aymanadel003@gmail.com>
2017-04-16 18:49:18 +0200

    generic sorting

--- Sorting/Sorting.swift
+++ Sorting/Sorting.swift
@@ -9,23 +9,32 @@
 import Foundation
 import UIKit
 
-func sort<T: Comparable> (array: inout[T]) {
-    for i in 1..<array.count {
-        var j = i - 1
-        let x = array[i]
-        var temp :T!
-        while j >= 0 && x < array[j] {
-            temp = array[j+1]
-            array[j+1] = array[j]
-            array[j] = temp
-            j -= 1
+class Sorting {
+    func sort<T: Comparable> (array: inout[T]) {
+        for i in 1..<array.count {
+            var j = i - 1
+            let x = array[i]
+            var temp :T!
+            while j >= 0 && x < array[j] {
+                temp = array[j+1]
+                array[j+1] = array[j]
+                array[j] = temp
+                j -= 1
+            }
+            array[j+1] = x
         }
-        array[j+1] = x
     }
+    
+    func test() {
+        
+        var array = [13, 30, 68, 19, 1, 4, 28, 65, 96, 13];
+        sort(array: &array)
+        
+        var chars = ["C","Z","O","A","B"]
+        sort(array: &chars)
+
+    }
+    
 }
 
-var array = [13, 30, 68, 19, 1, 4, 28, 65, 96, 13];
-sort(array: &array)
 
-var chars = ["C","Z","O","A","B"]
-sort(array: &chars)
