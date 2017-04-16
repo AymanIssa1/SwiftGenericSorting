9026b9183231fe4883a8f6fdf589290f90fcbd5a
ayman003 <aymanadel003@gmail.com>
2017-04-16 18:39:06 +0200

    Initial Commit

---
+++ Sorting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sorting
+//
+//  Created by LinuxPlus on 4/16/17.
+//  Copyright © 2017 LinuxPlus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
